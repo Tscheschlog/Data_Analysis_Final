42355dc42ed48875eebcea47ecf4a04f50d8abc8
Tscheschlog <chris.g.tscheschlog>
2023-11-30 17:30:20 -0500

    Update to county onclick logic

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const csv = require('csv-parser');
+const { spawn } = require('child_process');
 
 const app = express();
 const port = 3000;
@@ -24,8 +26,46 @@ const saveSearhedCounties = (searchedCounty, searchedEntities) => {
 
 app.get('api/:visual', (req, res) => {
   
-  
 
+});
+
+app.get('/api/counties_json', (req, res) => {
+  const filePath = path.join(__dirname, '/backend/data/florida_counties.json');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      const jsonData = JSON.parse(data);
+      res.json(jsonData);
+    }
+  });
+});
+
+app.get('/api/get_svg/:county', (req, res) => {
+  const county = req.query.county;
+
+  if (!county) {
+    return res.status(400).send('Please provide a county parameter.');
+  }
+
+  const pythonProcess = spawn('python', ['/backend/scripts/getCountySVG.py', county]);
+
+  let svgContent = '';
+
+  // Collect data from the Python script
+  pythonProcess.stdout.on('data', (data) => {
+    svgContent += data.toString();
+  });
+
+  pythonProcess.on('close', (code) => {
+    if (code === 0) {
+      res.type('image/svg+xml').send(svgContent);
+    } else {
+      res.status(500).send('Error running Python script');
+    }
+  });
 });
 
 // function convertCsvToJson(filePath, callback) {
--- public/scripts/mapSetup.js
+++ public/scripts/mapSetup.js
@@ -1,55 +1,81 @@
-fetch('../../backend/data/florida_counties.json')
-        .then(response => response.json())
-        .then(data => {
-            const map = L.map('map').setView([27.9944024, -81.760254], 7); // Set the initial view to Florida
 
-            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '© OpenStreetMap contributors'
-            }).addTo(map);
+const setVisualBackgroundImg = (county) => {
+    $('#visual-2').html(county);
+    console.log(county);
 
-            const defaultStyle = {
-                weight: 2,
-                color: 'green',
-                dashArray: '3',
-                fillOpacity: 0.2
-            };
+    fetch(`/api/get_svg/${county}`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(svgContent => {
+            // Use the SVG content as needed
+            console.log(svgContent);
+        })
+        .catch(error => {
+            console.error('Error fetching SVG:', error.message);
+        });
+}
 
-            L.geoJSON(data).addTo(map);
-            L.geoJSON(data, {
-                style: defaultStyle,
-                onEachFeature: function (feature, layer) {
-                    layer.on({
-                        mouseover: highlightFeature,
-                        mouseout: resetHighlight
-                    });
-                }
-            }).addTo(map);
+fetch('/api/counties_json')
+    .then(response => response.json())
+    .then(data => {
+        const map = L.map('map').setView([27.9944024, -81.760254], 7); // Set the initial view to Florida
 
-            // Function to highlight the feature (county)
-            function highlightFeature(e) {
-                const layer = e.target;
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '© OpenStreetMap contributors'
+        }).addTo(map);
 
-                const countyName = layer.feature.properties.NAMELSADCO;
-                layer.bindPopup(`<div class="county-popup">${countyName}</div>`, {}).openPopup();
+        const defaultStyle = {
+            weight: 2,
+            color: 'green',
+            dashArray: '3',
+            fillOpacity: 0.2
+        };
 
-                // console.log(layer.feature.properties);
-
-                layer.setStyle({
-                    weight: 2,
-                    color: '#eeff',
-                    dashArray: '3',
-                    fillOpacity: 0.5
+        L.geoJSON(data).addTo(map);
+        L.geoJSON(data, {
+            style: defaultStyle,
+            onEachFeature: function (feature, layer) {
+                layer.on({
+                    mouseover: highlightFeature,
+                    mouseout: resetHighlight,
+                    click: function (e) {
+                        setVisualBackgroundImg(layer.feature.properties.NAMELSADCO); 
+    
+                        console.log(layer.feature.properties.NAMELSADCO);
+                    }
                 });
-
-                if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-                    layer.bringToFront();
-                }
             }
+        }).addTo(map);
 
-            // Function to reset the highlight
-            function resetHighlight(e) {
-                const layer = e.target;
+        // Function to highlight the feature (county)
+        function highlightFeature(e) {
+            const layer = e.target;
 
-                layer.setStyle(defaultStyle);
+            const countyName = layer.feature.properties.NAMELSADCO;
+            layer.bindPopup(`<div class="county-popup">${countyName}</div>`, {}).openPopup();
+
+            // console.log(layer.feature.properties);
+
+            layer.setStyle({
+                weight: 2,
+                color: '#eeff',
+                dashArray: '3',
+                fillOpacity: 0.5
+            });
+
+            if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+                layer.bringToFront();
             }
+        }
+
+        // Function to reset the highlight
+        function resetHighlight(e) {
+            const layer = e.target;
+
+            layer.setStyle(defaultStyle);
+        }
 });   
